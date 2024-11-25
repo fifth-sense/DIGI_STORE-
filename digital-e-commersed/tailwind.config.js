d5ad54e654a7e50242aa909e1ab43e0e40086b93
Ruby <rubychoudhary1711@gmail.com>
2024-11-25 15:38:46 +0530

    retrotail ui setup

--- digital-e-commersed/tailwind.config.js
+++ digital-e-commersed/tailwind.config.js
@@ -17,11 +17,11 @@ module.exports = {
   			},
   			popover: {
   				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
+  				foreground: '#000'
   			},
   			primary: {
-  				DEFAULT: '#fbbf24',
-  				foreground: '#000000'
+  				DEFAULT: '#ffd726',
+  				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
   				DEFAULT: 'hsl(var(--secondary))',
@@ -54,6 +54,28 @@ module.exports = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
